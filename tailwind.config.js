e7fbf3b43759e6b362f91c68d0c856d1ca268c1e
Aleksan <sanyok.10rus@gmail.com>
2024-11-08 20:09:04 +0300

    added a half of content

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+    extend: {
+      fontFamily: {
+        "viaoda-libre-regular": ["Viaoda Libre", "serif"]
+      },
+      screens: {
+        'mobile': { 'raw': '(max-width: 639px)' },
+        'max-w-400': { 'raw': '(max-width: 400px)' },
+      }
+    },
+  },
+  plugins: [],
+}
+
